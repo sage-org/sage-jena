e986b3b0fa7d1023c33732ef0ef0e7504e6d1552
Thomas Minier <tminier01@gmail.com>
2018-04-27 13:37:36 +0200

    better SaGe model

--- src/main/java/org/gdd/sage/model/SageGraph.java
+++ src/main/java/org/gdd/sage/model/SageGraph.java
@@ -3,26 +3,42 @@ package org.gdd.sage.model;
 import org.apache.jena.graph.Triple;
 import org.apache.jena.graph.impl.GraphBase;
 import org.apache.jena.sparql.core.BasicPattern;
+import org.apache.jena.sparql.core.Substitute;
 import org.apache.jena.sparql.engine.QueryIterator;
 import org.apache.jena.util.iterator.ExtendedIterator;
+import org.apache.jena.util.iterator.WrappedIterator;
 import org.gdd.sage.engine.SageBGPIterator;
 import org.gdd.sage.http.SageRemoteClient;
 
+/**
+ * Represents a remote RDF graph hosted at a Sage server
+ * @author Thomas Minier
+ */
 public class SageGraph extends GraphBase {
-    private String url;
     private SageRemoteClient httpClient;
 
+    /**
+     * Constructor
+     * @param url - URL of the SaGe server
+     */
     public SageGraph(String url) {
         super();
-        this.url = url;
-        this.httpClient = new SageRemoteClient(this.url);
+        this.httpClient = new SageRemoteClient(url);
     }
 
     @Override
     protected ExtendedIterator<Triple> graphBaseFind(Triple triple) {
-        return null;
+        BasicPattern bgp = new BasicPattern();
+        bgp.add(triple);
+        QueryIterator queryIterator = new SageBGPIterator(httpClient, bgp);
+        return WrappedIterator.create(queryIterator).mapWith(binding -> Substitute.substitute(triple, binding));
     }
 
+    /**
+     * Evaluate a Basic Graph Pattern using the SaGe server
+     * @param bgp - BGP to evaluate
+     * @return An iterator over solution bindings for the BGP
+     */
     public QueryIterator evaluateBGP(BasicPattern bgp) {
         return new SageBGPIterator(httpClient, bgp);
     }
--- src/main/java/org/gdd/sage/model/SageModelFactory.java
+++ src/main/java/org/gdd/sage/model/SageModelFactory.java
@@ -3,9 +3,11 @@ package org.gdd.sage.model;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 
+/**
+ * Helper factory used to create Jena Models connected to a remote SaGe server
+ */
 public class SageModelFactory {
-
-    public static final Model createModel(String url) {
+    public static Model createModel(String url) {
         return ModelFactory.createModelForGraph(new SageGraph(url));
     }
 }
