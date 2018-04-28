dd45d1c1cedad2ecceb747c35b49f0fb24b30914
Thomas Minier <tminier01@gmail.com>
2018-04-28 17:52:57 +0200

    add tooling for tests

---
+++ src/test/java/org/gdd/sage/http/SageRemoteClientTest.java
@@ -0,0 +1,13 @@
+package org.gdd.sage.http;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SageRemoteClientTest {
+
+    @Test
+    public void query() {
+        assertTrue(true);
+    }
+}
