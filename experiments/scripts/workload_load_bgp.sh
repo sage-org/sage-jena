9e324173ff3cabeebeec7b9dcddef38a10bc3386
Thomas Minier <tminier01@gmail.com>
2018-07-19 10:05:43 +0200

    update tpf script

--- experiments/scripts/workload_load_bgp.sh
+++ experiments/scripts/workload_load_bgp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NBRUNS=(1 2 3)
-NBCLIENTS=(1 2 3 4 5 6 7 8 9 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 95 100)
+NBCLIENTS=(1 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100)
 
 for run in ${NBRUNS[@]}; do
   RESDIR="/home/minier/www19-watdiv-sage-75ms/run${run}"
--- experiments/scripts/workload_load_ref.sh
+++ experiments/scripts/workload_load_ref.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 NBRUNS=(1 2 3)
-NBCLIENTS=(1 2 3 4 5 6 7 8 9 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 95 100)
+NBCLIENTS=(1 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100)
 
 for run in ${NBRUNS[@]}; do
-  RESDIR="/home/minier/watdiv-ref-load/run${run}"
+  RESDIR="/home/minier/www19-watdiv-tpf/run${run}"
   mkdir -p $RESDIR
   for nb in ${NBCLIENTS[@]}; do
-    python3 scripts/run_load.py watDivQueries/ watDivQueries/query_10175.rq $RESDIR /home/minier/watdiv-reference/results/ $nb ref
-    mv $RESDIR/execution_times_ref.csv $RESDIR/execution_times_${nb}c.csv
+    python3 scripts/run_load.py watDivQueries/ load_watdiv.sparql $RESDIR /home/minier/watdiv-reference/results/ $nb ref
+    mv $RESDIR/execution_times_tpf.csv $RESDIR/execution_times_${nb}c.csv
   done
 done
